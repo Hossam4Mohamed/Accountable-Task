ae1109e64da3e6e90d7f206ee423bcaf0378ac8a
Hossam El-Adawy <eng.hossam4mohamed@gmail.com>
2021-07-16 00:04:22 +0200

    initialize unit testing using jest

---
+++ BusinessLogic/src/middleware/middleware.test.ts
@@ -0,0 +1,13 @@
+// import { Request, Response, NextFunction } from 'express';
+
+// import { isWorkerAuthorized } from './authorizeWorker';
+
+// describe('Middleware Functions', () => {
+//   let mockReq: Partial<Request>;
+//   let mockR: Partial<Request>;
+//   let mockRequest: Partial<Request>;
+
+//   test('dasd', async () => {
+//     await isWorkerAuthorized();
+//   });
+// });
